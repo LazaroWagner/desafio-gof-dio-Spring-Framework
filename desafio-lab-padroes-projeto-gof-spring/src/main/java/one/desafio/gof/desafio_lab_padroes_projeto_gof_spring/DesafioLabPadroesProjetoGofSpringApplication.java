4ed07bb7c70f769a0e1a7bbdcc8d59467d0a9739
Lazaro Marinho <lazarowagner@hotmail.com>
2026-03-10 08:24:32 -0300

    Start in Projet

---
+++ desafio-lab-padroes-projeto-gof-spring/src/main/java/one/desafio/gof/desafio_lab_padroes_projeto_gof_spring/DesafioLabPadroesProjetoGofSpringApplication.java
@@ -0,0 +1,13 @@
+package one.desafio.gof.desafio_lab_padroes_projeto_gof_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioLabPadroesProjetoGofSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioLabPadroesProjetoGofSpringApplication.class, args);
+	}
+
+}
---
+++ desafio-lab-padroes-projeto-gof-spring/src/test/java/one/desafio/gof/desafio_lab_padroes_projeto_gof_spring/DesafioLabPadroesProjetoGofSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package one.desafio.gof.desafio_lab_padroes_projeto_gof_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioLabPadroesProjetoGofSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
